a7df931cf97dbead5955b343d59ab0c894b1fed1
Carl W. Irving <cwirving@scroogieboy.com>
2024-12-29 22:56:26 -0600

    Closing in on 0.8.1

--- constants.ts
+++
@@ -1,5 +0,0 @@
-// The URL protocol we'll use for cached zip files.
-export const CACHED_ZIP_FILE_PROTOCOL = "czf:";
-
-// The default amount of time the ZipReader will remember zip archives, in milliseconds.
-export const DEFAULT_ARCHIVE_TTL = 10000;
